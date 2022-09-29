b39f820794388adb6f830d52e4844ac00f63096d
LeahKupfer <s01221525@acad.tri-c.edu>
2022-09-29 15:42:03 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using LeahSporting.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LeahSporting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.TaxAmount = 0;
+            ViewBag.TotalSalePrice = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Sale productSale)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.TaxAmount = productSale.TaxAmount();
+                ViewBag.TotalSalePrice = productSale.TotalSalePrice();
+            }
+            else
+            {
+                ViewBag.TaxAmount = 0;
+                ViewBag.TotalSalePrice = 0;
+            }
+          
+            return View(productSale);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
